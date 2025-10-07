baa4e41f9b1e1befc9c956703b2fc3aac89d8302
Franck NIAT <niatmanuel2002@gmail.com>
2025-10-07 14:57:23 +0100

    feat: configuration avancée de l'authentification et de la base de données
    - activation de la vérification email obligatoire avec Resend
    - configuration complète de la base de données Neon avec schéma
    - intégration des données réelles dans la page principale
    - ajout du plugin openAPI pour better-auth

--- src/app/(todo)/page.tsx
+++ src/app/(todo)/page.tsx
@@ -1,9 +1,16 @@
+import { getTasks } from "@/actions/task";
 import TodoList from "@/components/todo/todo-list";
+import { getSession } from "@/lib/auth";
+import { headers } from "next/headers";
 
-export default function Home() {
-  return(
-    <div className=" py-6 px-2">
-      <TodoList tasks={[]} />
-    </div>
-  )
+export default async function Home() {
+	const session = await getSession({
+		headers: await headers(),
+	});
+	const tasks = await getTasks(session?.user.id);
+	return (
+		<div className=" py-6 px-2">
+			<TodoList tasks={tasks} />
+		</div>
+	);
 }
