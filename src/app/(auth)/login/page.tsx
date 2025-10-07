0d028dee0802b7e52b2336b4b4f5922c16713f65
Franck NIAT <niatmanuel2002@gmail.com>
2025-10-07 08:39:39 +0100

    fix: ajout du Suspense autour du LoginForm

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 import { LoginForm } from "@/components/auth/login-form";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
-    </div>
+    </Suspense>
   )
 }
