8331af912bcca733469a42587db0c28383eb8850
Franck NIAT <niatmanuel2002@gmail.com>
2025-10-07 08:37:23 +0100

    fix: afficher les tâches dans la page principale

--- src/app/(todo)/page.tsx
+++ src/app/(todo)/page.tsx
@@ -3,7 +3,7 @@ import TodoList from "@/components/todo/todo-list";
 export default function Home() {
   return(
     <div className=" py-6 px-2">
-      <TodoList />
+      <TodoList tasks={[]} />
     </div>
   )
 }
