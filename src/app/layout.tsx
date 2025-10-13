3bee1012451fccf54af0632c5f10765010a88c09
Franck NIAT <niatmanuel2002@gmail.com>
2025-10-13 12:01:57 +0100

    feat: ajout d'un outil d'analyse web (Vercel analytics)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers";
 import Navbar from "@/components/layouts/navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.className} ${geistMono.variable} antialiased`}
 			>
+				<Analytics />
 				<Providers>
 					<Navbar />
 					<div className="max-w-[700px] mx-auto px-4 pt-20">
